dcd9d9b4336491d28afb248eaaeb44acc889ad6a
Yash Lala <yashlala@gmail.com>
2021-01-23 09:46:04 -0800

    Implement initial RTT measurements.
    We can now get circuit RTT for Google!
    Unfortunately, getting the precise circuits looks like a pain.
    Let's switch to another piece of code from a sample guide.

--- rtt.py
+++ rtt.py
@@ -1,9 +1,15 @@
 import time
 import sys
 
+from stem import Signal
+from stem.control import Controller
 import requests
 
-def find_RTT(target_url): 
+# signal TOR for a new connection 
+def renew_tor_circuit(controller):
+    controller.signal(Signal.NEWNYM)
+
+def find_circuit_RTT(target_url): 
     with requests.Session() as session: 
         # Connect to TOR's default SOCKS port. 
         session.proxies = {'http':  'socks5://127.0.0.1:9050',
@@ -15,4 +21,6 @@ def find_RTT(target_url):
 
 
 if __name__ == '__main__': 
-    print(find_RTT('http://google.com/'))
+    controller =  Controller.from_port(port=tor_port)
+    controller.authenticate()
+    print(find_circuit_RTT('http://bbc.co.uk/'))
