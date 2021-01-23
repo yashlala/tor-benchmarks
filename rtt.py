e15e3ed8d915f66870b576bb77a30f822eda9425
Yash Lala <yashlala@gmail.com>
2021-01-23 10:30:35 -0800

    Adapt custom-relay TOR circuit.

--- rtt.py
+++ rtt.py
@@ -1,26 +1,78 @@
+import io
 import time
-import sys
 
-from stem import Signal
-from stem.control import Controller
-import requests
+import pycurl
 
-# signal TOR for a new connection 
-def renew_tor_circuit(controller):
-    controller.signal(Signal.NEWNYM)
+import stem.control
 
-def find_circuit_RTT(target_url): 
-    with requests.Session() as session: 
-        # Connect to TOR's default SOCKS port. 
-        session.proxies = {'http':  'socks5://127.0.0.1:9050',
-                           'https': 'socks5://127.0.0.1:9050'}
+# Static exit for us to make 2-hop circuits through. Picking aurora, a
+# particularly beefy one...
+#
+#   https://metrics.torproject.org/rs.html#details/379FB450010D17078B3766C2273303C358C3A442
 
-        response = session.head(target_url)
-        assert response.ok
-        return response.elapsed
+EXIT_FINGERPRINT = '379FB450010D17078B3766C2273303C358C3A442'
+
+SOCKS_PORT = 9050
+CONNECTION_TIMEOUT = 30
+
+def query_RTT(url):
+    """Use pycurl to fetch a site using the proxy on the SOCKS_PORT."""
+
+    output = open('/dev/null', 'wb')
+
+    query = pycurl.Curl()
+    query.setopt(pycurl.URL, url)
+    query.setopt(pycurl.PROXY, 'localhost')
+    query.setopt(pycurl.PROXYPORT, SOCKS_PORT)
+    query.setopt(pycurl.PROXYTYPE, pycurl.PROXYTYPE_SOCKS5_HOSTNAME)
+    query.setopt(pycurl.NOBODY, True) # Send HTTP HEAD requests (no msg body).
+    query.setopt(pycurl.CONNECTTIMEOUT, CONNECTION_TIMEOUT)
+    query.setopt(pycurl.WRITEFUNCTION, output.write)
+
+    try:
+        query.perform()
+
+        return query.getinfo(pycurl.CONNECT_TIME)
+    except pycurl.error as exc:
+        raise ValueError(f"Unable to reach {url} ({exc})")
 
 
-if __name__ == '__main__': 
-    controller =  Controller.from_port(port=tor_port)
-    controller.authenticate()
-    print(find_circuit_RTT('http://bbc.co.uk/'))
+def scan(controller, path):
+    """
+    Fetch check.torproject.org through the given path of relays, providing back
+    the time it took.
+    """
+
+    circuit_id = controller.new_circuit(path, await_build=True)
+
+    def attach_stream(stream):
+        if stream.status == 'NEW':
+            controller.attach_stream(stream.id, circuit_id)
+    controller.add_event_listener(attach_stream, stem.control.EventType.STREAM)
+
+    try:
+        controller.set_conf('__LeaveStreamsUnattached', '1') 
+        start_time = time.time()
+
+        check_page = query('https://check.torproject.org/')
+
+        if 'Congratulations. This browser is configured to use Tor.' not in check_page:
+            raise ValueError("Request didn't have the right content")
+
+        return time.time() - start_time
+    finally:
+        controller.remove_event_listener(attach_stream)
+        controller.reset_conf('__LeaveStreamsUnattached')
+
+
+    with stem.control.Controller.from_port() as controller:
+        controller.authenticate()
+
+    relay_fingerprints = [desc.fingerprint for desc in controller.get_network_statuses()]
+
+    for fingerprint in relay_fingerprints:
+        try:
+            time_taken = scan(controller, [fingerprint, EXIT_FINGERPRINT])
+        print('%s => %0.2f seconds' % (fingerprint, time_taken))
+        except Exception as exc:
+            print('%s => %s' % (fingerprint, exc))
