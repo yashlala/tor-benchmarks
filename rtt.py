304be7c094b5afbef88c6a51fb37aff76521b752
Yash Lala <yashlala@gmail.com>
2021-02-03 09:24:32 -0800

    Refactor rtt code.

--- rtt.py
+++ rtt.py
@@ -1,4 +1,5 @@
 import io
+import itertools
 
 import pycurl
 import stem.control
@@ -9,11 +10,15 @@ import stem.control
 EXIT_FINGERPRINT = '376DC7CAD597D3A4CBB651999CFAD0E77DC9AE8C'
 
 SOCKS_PORT = 9050
-CONNECTION_TIMEOUT = 30
+CONNECTION_TIMEOUT = 10
+
+URL = 'https://google.com/'
+# URL = "http://104.154.161.181/"
 
 def query(url):
     """Use pycurl to fetch a site using the proxy on the SOCKS_PORT."""
 
+    # Discard webpage output. Should be empty anyways. 
     output = open('/dev/null', 'wb')
 
     query = pycurl.Curl()
@@ -28,46 +33,37 @@ def query(url):
     try:
         query.perform()
     except pycurl.error as exc:
-        raise ValueError(f"Unable to reach {url} ({exc})")
+        raise ConnectionError(f"Unable to reach {url} ({exc})")
 
     return query.getinfo(pycurl.CONNECT_TIME)
 
 
 def scan(controller, path):
-    """
-    Fetch check.torproject.org through the given path of relays, providing back
-    the time it took.
-    """
-
-    # Set up a new circuit, blocking until the circuit is set up. 
-    circuit_id = controller.new_circuit(path, await_build=True)
-
-    def attach_stream(stream):
-        if stream.status == 'NEW':
-            controller.attach_stream(stream.id, circuit_id)
-    controller.add_event_listener(attach_stream, stem.control.EventType.STREAM)
+    """Fetch a URL through the given relay path."""
 
     try:
+        # Set up a new circuit, blocking until the circuit is set up. 
+        circuit_id = controller.new_circuit(path, await_build=True)
+
+        def attach_stream(stream):
+            if stream.status == 'NEW':
+                controller.attach_stream(stream.id, circuit_id)
+        controller.add_event_listener(attach_stream, stem.control.EventType.STREAM)
+
         controller.set_conf('__LeaveStreamsUnattached', '1') 
-
-        rtt = query('https://check.torproject.org/')
-
-        if 'Congratulations. This browser is configured to use Tor.' not in check_page:
-            raise ValueError("Request didn't have the right content")
-
+        rtt = query(URL)
     finally:
         controller.remove_event_listener(attach_stream)
         controller.reset_conf('__LeaveStreamsUnattached')
+        controller.close_circuit(circuit_id)
+
+    return rtt
 
 
 with stem.control.Controller.from_port() as controller:
+    print('address,rtt')
     controller.authenticate()
 
-    relay_fingerprints = [desc.fingerprint for desc in controller.get_network_statuses()]
-
-    for fingerprint in relay_fingerprints:
-        try:
-            time_taken = scan(controller, [fingerprint, EXIT_FINGERPRINT])
-            print('%s => %0.2f seconds' % (fingerprint, time_taken))
-        except Exception as exc:
-            print('%s => %s' % (fingerprint, exc))
+    for relay in itertools.islice(controller.get_network_statuses(), 50): 
+        rtt = scan(controller, [relay.fingerprint, EXIT_FINGERPRINT])
+        print(f'{relay.address},{rtt}')
